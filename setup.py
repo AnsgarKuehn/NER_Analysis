de9cbee3cfc9eaf9bcf01951543385ac54b0d8c5
Ansgar Kühn <ansgar.khn@stud.uni-goettingen.de>
2021-06-15 16:22:00 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Small resume project utilizing the hugginface library for nlp with focus on structure',
+    author='Ansgar Kühn',
+    license='MIT',
+)
